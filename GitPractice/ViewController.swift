ab33db11ba426a4ab0d524bcc0dcd6e14ab9eb6e
prasiddhbarot24 <prasiddhrao12345@gmail.com>
2024-06-18 11:46:52 +0530

    print hello message

--- GitPractice/ViewController.swift
+++ GitPractice/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("hell git!......")
+    }
 
 }
 
